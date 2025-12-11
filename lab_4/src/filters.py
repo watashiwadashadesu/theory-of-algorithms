c4bac4c2cd0eed78244b1aae7ee8666c3fccf1fa
daria <konstantinova1dar1a@gmail.com>
2025-12-11 13:44:07 +0300

    Remove lab_4 and .DS_Store

--- lab_4/src/filters.py
+++
@@ -1,8 +0,0 @@
-def filter_two_digits(numbers):
-    try:
-        nums = list(map(int, numbers.split()))
-        return list(filter(lambda x: 10 <= abs(x) <= 99, nums))
-    except ValueError:
-        raise ValueError("Введены нечисловые данные")
-    except Exception as e:
-        raise RuntimeError("Ошибка фильтрации") from e
